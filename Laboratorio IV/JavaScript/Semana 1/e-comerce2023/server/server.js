7f78316dddedbb542a289758316a1e1810030735
lbritez <lbritez@riviera-networks.com>
2023-09-20 20:55:02 +0200

    4.2 Comenzamos con el método de pago -> Parte 7 -> Probamos los pagos

--- Laboratorio IV/JavaScript/Semana 1/e-comerce2023/server/server.js
+++ Laboratorio IV/JavaScript/Semana 1/e-comerce2023/server/server.js
@@ -1,22 +1,22 @@
-const express = require("express"); //express: framework para node js
-const app = express(); //esta constante inicializa express
-const cors = require("cors"); //medida de seguridad para navegadores
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mercadopago = require("mercadopago");
-const path = require("path"); //Direccion a los directorios necesarios para arrancar el servidor
+const path = require("path");
 
 // REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
 mercadopago.configure({
-    access_token: "<ACCESS_TOKEN>",
+    access_token: "APP_USR-8005534317430798-091718-b982966f21e5ad67337aec6868ccafbb-1483636744",
 });
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "../client")));
 app.use(cors());
 
 app.get("/", function () {
-    res.sendFile(path.resolve(__dirname, "..", "client", "index.html"));
+    path.resolve(__dirname, "..", "client", "index.html");
 });
 
 app.post("/create_preference", (req, res) => {
@@ -30,24 +30,22 @@ app.post("/create_preference", (req, res) => {
             }
         ],
         back_urls: {
-            "success": "http://localhost:8080", //si la compra se realizó con éxito, redirige al servidor localhost:8080
-            "failure": "http://localhost:8080", //si falla, volvemos a la página principal
-            "pending": "" //si está pendiente queda en blanco, NO lo usaremos
+            "success": "http://localhost:8080",
+            "failure": "http://localhost:8080",
+            "pending": ""
         },
-        auto_return: "approved", //te retorna al servidor
+        auto_return: "approved",
     };
-
     mercadopago.preferences.create(preference)
         .then(function (response) {
             res.json({
                 id: response.body.id
             });
-        }).catch(function (error) {
-        console.log(error);
-    });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 });
-
-//CONFIGURACIONES AVANZADAS SOBRE EL ESTADO DEL PAGO -> NO LAS USAREMOS
 app.get('/feedback', function (req, res) {
     res.json({
         Payment: req.query.payment_id,
@@ -55,7 +53,6 @@ app.get('/feedback', function (req, res) {
         MerchantOrder: req.query.merchant_order_id
     });
 });
-
 app.listen(8080, () => {
     console.log("The server is now running on Port 8080");
 });
