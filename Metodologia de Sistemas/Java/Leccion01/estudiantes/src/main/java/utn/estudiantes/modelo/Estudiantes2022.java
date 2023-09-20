9bb6b95ae90a1bae14237062edeb0e4ddec2120a
Leo <tengopaye@gmail.com>
2023-09-20 00:39:14 -0300

    Java App estudiante parte 4

--- Metodologia de Sistemas/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2022.java
+++ Metodologia de Sistemas/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2022.java
@@ -1,4 +1,5 @@
 package utn.estudiantes.modelo;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,19 +9,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
-//boilerplate - Repetitivo
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class Estudiante {
-    @Id
+
+@Entity //Indica que la clase es una clase entidad = Tabla de datos
+
+//boilerplate - CodigoRepetitivo  (Son los codigos de constructores)
+@Data //Genera los metodos get y set
+@NoArgsConstructor //Constructor sin argumetos
+@AllArgsConstructor //Constructor con todos los argumentos
+@ToString //Metodos to String
+public class Estudiantes2022 {
+    @Id // Agrega el orden de llave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEstudiante;
     private String nombre;
     private String apellido;
     private String telefono;
     private String email;
-
 }
--- Metodologia de Sistemas/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Metodologia de Sistemas/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,34 +1,36 @@
 package utn.estudiantes.servicio;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2022;
 import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
 @Service
-public class EstudianteServicio implements IEstudianteServicio {
+public class EstudianteServicio implements IEstudianteServicio{
     @Autowired
-    private EstudianteRepositorio estudianteRepositorio;
+    private EstudianteRepositorio estudianteRepositorio; //es el objeto que traemos
 
     @Override
-    public List<Estudiante> listarEstudiantes(){
-        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
-        return  estudiantes;
-    }
-    @Override
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante){
-        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
-        return estudiante;
+    public List<Estudiantes2022> listarEstudiantes() { //Método listarEstudiantes - video 1
+        List<Estudiantes2022> estudiantes = estudianteRepositorio.findAll(); //nos trae toda la lista de estudiantes
+        return estudiantes;
     }
 
     @Override
-    public void guardarEstudiante(Estudiante estudiante){
+    public Estudiantes2022 buscarEstudiantePorId(Integer idEstudiantes2022) {
+        Estudiantes2022 estudiante = estudianteRepositorio.findById(idEstudiantes2022).orElse(null); // findById es opcional si norecibe el id o regresa en este caso un null
+        return  estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiantes2022 estudiante) {
         estudianteRepositorio.save(estudiante);
     }
 
-    public void eliminarEstudiante(Estudiante estudiante) {
+    @Override
+    public void eliminarEstudiante(Estudiantes2022 estudiante) {
         estudianteRepositorio.delete(estudiante);
-    }
-
+    } //esta clase implementa la interface
 }
