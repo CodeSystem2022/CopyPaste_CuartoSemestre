8de6c29f024a96a499238af1da37f55d37298393
lbritez <lbritez@riviera-networks.com>
2023-09-20 14:04:50 +0200

    4.2 Comenzamos con el método de pago -> Parte 4 -> Integrar mercado pago desde el frontend

--- Laboratorio IV/JavaScript/Semana 1/e-comerce2023/server/server.js
+++ Laboratorio IV/JavaScript/Semana 1/e-comerce2023/server/server.js
@@ -2,32 +2,31 @@ const express = require("express"); //express: framework para node js
 const app = express(); //esta constante inicializa express
 const cors = require("cors"); //medida de seguridad para navegadores
 const mercadopago = require("mercadopago");
+const path = require("path"); //Direccion a los directorios necesarios para arrancar el servidor
 
 // REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
 mercadopago.configure({
-    access_token: "<ACCESS_TOKEN>", //las creamos desde mercado pago después
+    access_token: "<ACCESS_TOKEN>",
 });
-
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); //para que pueda leer archivos .json
-app.use(express.static("../../client/html-js")); //no redirige a este archivo estático
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../../client")));
 app.use(cors());
 
-//inicializa una primer ruta vacía "/"
-app.get("/", function (req, res) {
-    res.status(200).sendFile("index.html"); //va a mostrar lo del index.html
+app.get("/", function () {
+    path.resolve(__dirname,"..", "client", "index.html");
 });
 
-//al mandar desde el front-end los datos de una compra, se crea una preferencia
 app.post("/create_preference", (req, res) => {
 
     let preference = {
-        items: [ //con determinados items
+        items: [
             {
-                title: req.body.description, //título
-                unit_price: Number(req.body.price), //precio y desde donde lo trae
-                quantity: Number(req.body.quantity), //cantidad
+                title: req.body.description,
+                unit_price: Number(req.body.price),
+                quantity: Number(req.body.quantity),
             }
         ],
         back_urls: {
@@ -40,19 +39,19 @@ app.post("/create_preference", (req, res) => {
 
     mercadopago.preferences.create(preference)
         .then(function (response) {
-            res.json({ //se manda al back-end la respuesta en formato json
-                id: response.body.id //se genera un ID para esa compra que se generó en el front-end
-            }); //ese id mostrará la información de la compra en la pasarela de pago
-        }).catch(function (error) { //usamos catch para manejo de errores
+            res.json({
+                id: response.body.id
+            });
+        }).catch(function (error) {
         console.log(error);
     });
 });
 
 //CONFIGURACIONES AVANZADAS SOBRE EL ESTADO DEL PAGO -> NO LAS USAREMOS
-app.get('/feedback', function (req, res) { //para dar mas feedback en caso de configurar mas cosas en mercado pago
+app.get('/feedback', function (req, res) {
     res.json({
-        Payment: req.query.payment_id, //seguir el estado del pago
-        Status: req.query.status, //seguir el estado de la compra
+        Payment: req.query.payment_id,
+        Status: req.query.status,
         MerchantOrder: req.query.merchant_order_id
     });
 });
