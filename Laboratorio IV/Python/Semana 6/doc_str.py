351a14770b9d797c232b35c8857c66fa1c9068b1
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-09-24 16:43:47 +0200

    5.2 Dunder doc (__doc__) -> parte 2

--- Laboratorio IV/Python/Semana 6/doc_str.py
+++ Laboratorio IV/Python/Semana 6/doc_str.py
@@ -2,6 +2,13 @@ from mi_clase import MiClase
 
 # help devuelve la documentacion de la clase
 # help(MiClase)
-# help(MiClase.)  con el punto muestra los métodos tambien
+
+# help con el punto muestra los métodos tambien
+# help(MiClase.)  
 
 print(MiClase.__doc__) 
+# print(MiClase.__doc__)
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)  # cada metodo en si mismo es un objet
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
