a3a964daa2659d4cddf8fa2f6106a448a4859bf4
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-09-24 19:16:26 +0200

    5.5 Método split cadena a lista -> Parte 1

---
+++ Laboratorio IV/Python/Semana 6/metodo_split.py
@@ -0,0 +1,8 @@
+# metodo split sginifiva dividir/separar
+
+# help(str.split)
+
+cursos = 'Java Javascript Node Python Diseno'
+lista_cursos = cursos.split()
+print(f'Lista de cursos: {lista_cursos}')
+print(type(lista_cursos))
