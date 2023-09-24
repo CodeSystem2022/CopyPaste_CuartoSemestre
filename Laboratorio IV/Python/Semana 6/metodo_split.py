80f4e61f60332b78e7e4a58cc0a5cc9cffa6a79b
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-09-24 19:19:47 +0200

    5.5 Método split cadena a lista -> Parte 2

--- Laboratorio IV/Python/Semana 6/metodo_split.py
+++ Laboratorio IV/Python/Semana 6/metodo_split.py
@@ -1,4 +1,4 @@
-# metodo split sginifiva dividir/separar
+# El metodo split sginifiva dividir/separar
 
 # help(str.split)
 
@@ -6,3 +6,8 @@ cursos = 'Java Javascript Node Python Diseno'
 lista_cursos = cursos.split()
 print(f'Lista de cursos: {lista_cursos}')
 print(type(lista_cursos))
+
+cursos_separados_coma = 'Java,Python,Node,Javascript,Spring'
+lista_cursos = cursos_separados_coma.split(',', 2) # Busca la separacion de , solo 2 veces
+print(f'Lista de cursos: {lista_cursos}')
+print(len(lista_cursos))
