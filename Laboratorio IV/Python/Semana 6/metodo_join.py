1f5af326e0d43ed8406976b11019b06bab015c55
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-09-24 19:05:42 +0200

    5.4 Método join para colecciones a cadenas -> Parte 1

---
+++ Laboratorio IV/Python/Semana 6/metodo_join.py
@@ -0,0 +1,7 @@
+# El método join une a los elementos
+
+# help(str.join)
+
+tupla_str = ('Hola', 'alumnos', 'Tecnicatura', 'Universitaria') # Tupla inicial de 4 elementos
+mensaje = ' '.join(tupla_str) # Crea una nueva cadena uniendo espacios entre las palabras llamada mensaje
+print(f'Mensaje: {mensaje}') 
