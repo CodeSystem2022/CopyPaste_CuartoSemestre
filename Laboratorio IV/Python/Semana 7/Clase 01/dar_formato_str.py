45750372f2b5f406ce6da0fac594b03b02460f76
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-10-04 18:02:24 +0200

    6.3 Uso del método format() -> utilizamos place holder

--- Laboratorio IV/Python/Semana 7/Clase 01/dar_formato_str.py
+++ Laboratorio IV/Python/Semana 7/Clase 01/dar_formato_str.py
@@ -8,4 +8,11 @@ mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
 #Creamos una tupla
 persona = ('Noelia', 'Ruiz', 5000.00)
 mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f' #% persona #le pasamos la tupla persona
-print(mensaje_con_formato % persona) #podemos pasarlo en el print directamente
+# print(mensaje_con_formato % persona) #podemos pasarlo en el print directamente
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
