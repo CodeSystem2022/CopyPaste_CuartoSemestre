5535148a4a4730119fbeebbeef190cb45b9f37b0
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-11-10 00:37:57 +0100

    11.4 Vamos a la ejecución -> análisis y errores

--- Laboratorio IV/Python/Semana 13/chart/chart.py
+++ Laboratorio IV/Python/Semana 13/chart/chart.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt #alias
 def generate_bar_chart(name, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.savefig(f'pie.png')
+    plt.savefig('pie.png')
     plt.close()
 
 def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig(f'bar.png')
+    plt.savefig('bar.png')
     plt.close()
 
 if __name__ == '__main__':
