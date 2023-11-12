c96acac44e3297359c6ab8e0028161c2de4a0e85
jesica-viola <jesicaviola21@gmail.com>
2023-11-12 20:39:54 -0300

    Ultimo de

--- Metodologia de Sistemas/Node Js/02-Fundamentos_node/modulos/globales.js
+++ Metodologia de Sistemas/Node Js/02-Fundamentos_node/modulos/globales.js
@@ -19,4 +19,23 @@
 //setImmdiate();
 
 //console.log(global);
-console.log(setInterval); // es una función
+//console.log(setInterval); // es una función
+
+
+
+let i = 0;
+let intervalo = setInterval(() => {
+    console.log('Hola');
+    if (i ===3){ 
+    clearInterval(intervalo); //detenemos la funcion
+}
+i++;
+}, 1000);
+
+setImmediate(() => {
+    console.log(`Saludo inmediato`);
+});
+
+//require();
+
+console.log(__filename);
